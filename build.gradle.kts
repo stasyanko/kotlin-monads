9e468a9d7ac3131a9601b01c117b5de75aac5a8f
Stanislav Yankovskiy <s.yankovskiy@lavinmedia.com>
2022-11-17 14:11:55 +0300

    start

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,9 @@ repositories {
 }
 
 dependencies {
+    implementation("io.arrow-kt:arrow-core:1.1.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,38 @@
-fun main(args: Array<String>) {
+import arrow.core.Either
+import arrow.core.Either.Left
+import arrow.core.continuations.either
+import kotlinx.coroutines.runBlocking
 
+object NotFound
+
+class Speaker {
+    fun getTalk(): Either<NotFound, Talk> = Left(NotFound)
+}
+
+class Talk {
+    fun getConference(): Either<NotFound, Conference> = Left(NotFound)
+}
+
+class City
+
+class Conference {
+    fun getCity(): Either<NotFound, City> = Left(NotFound)
+}
+
+suspend fun cityToVisit(speaker: Speaker): Either<NotFound, Conference> = either{
+    val s = speaker.getTalk().bind()
+    s.getConference().bind()
+}
+
+
+fun main() = runBlocking {
+    val city = cityToVisit(Speaker())
+    when(city) {
+        is Left -> {
+            println("Error: " + city.value)
+        }
+        is Either.Right -> {
+            println("City:" + city.value)
+        }
+    }
 }
